4eab4bfa9266e95a0922a08a29f699ba400cb3f9
Okke Tijhuis <okke.tijhuis@eno.nl>
2013-06-03 01:02:01 +0200

    clojure-test-mode deleted because of paredit-mode not starting anymore after upgrade

---
+++ elpa/evil-20130601.1704/evil-pkg.el
@@ -0,0 +1,3 @@
+(define-package "evil" "20130601.1704" "Extensible Vi layer for Emacs. [git]"
+  '((undo-tree "0.5.1")))
+
---
+++ elpa/evil-leader-20130530.1519/evil-leader-pkg.el
@@ -0,0 +1,1 @@
+(define-package "evil-leader" "20130530.1519" "let there be <leader> [github]" (quote ((evil "0"))))
