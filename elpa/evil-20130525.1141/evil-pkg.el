ddcc93f247823233827a41c64721d47e062f0e21
Okke Tijhuis <okke.tijhuis@eno.nl>
2013-06-03 20:40:39 +0200

    package upgrades

--- elpa/evil-20130525.1141/evil-pkg.el
+++
@@ -1,3 +0,0 @@
-(define-package "evil" "20130525.1141" "Extensible Vi layer for Emacs. [git]"
-  '((undo-tree "0.5.1")))
-
--- elpa/evil-leader-20130520.1514/evil-leader-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "evil-leader" "20130520.1514" "let there be <leader> [github]" (quote ((evil "0"))))
