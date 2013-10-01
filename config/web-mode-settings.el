9084f38f22124315325e1822d52aa11fc7459e1e
Okke Tijhuis <okke.tijhuis@eno.nl>
2013-10-01 11:50:35 +0200

    web-mode settings

--- config/web-mode-settings.el
+++ config/web-mode-settings.el
@@ -15,6 +15,8 @@
     (setq web-mode-markup-indent-offset 2)
     (setq web-mode-css-indent-offset 2)
     (setq web-mode-code-indent-offset 2)
+    (setq web-mode-indent-style 2)
+    (setq web-mode-enable-current-element-highlight t)
     (idle-highlight-mode 0)
     (font-lock-mode 0))
 
