16bf6e1eaf226b6b8b16be1bf6b76221c39a35af
Okke Tijhuis <okke.tijhuis@eno.nl>
2013-10-27 20:29:43 +0100

    cider tweaks

--- config/cider-settings.el
+++ config/cider-settings.el
@@ -15,14 +15,15 @@
 (setq nrepl-hide-special-buffers t)
 ;(setq nrepl-popup-stacktraces-in-repl t)
 
+(setq cider-repl-pop-to-buffer-on-connect nil) ; Prevent the auto-display of the REPL buffer in a separate window after connection is established
+
 (eval-after-load "cider"
   '(progn
         (setq cider-popup-stacktraces nil) ;Stop the error buffer from popping up while working in the REPL buffer
-        (add-to-list 'same-window-buffer-names "*nrepl*") ;Make C-c C-z switch to the *nrepl* buffer in the current window
+        (add-to-list 'same-window-buffer-names "*cider*") ;Make C-c C-z switch to the *nrepl* buffer in the current window
      ))
 
-(add-hook 'cider-mode-hook
-  'cider-turn-on-eldoc-mode) ; Enable eldoc in clojure buffers
+(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode) ; Enable eldoc in clojure buffers
 (add-hook 'cider-repl-mode-hook 'subword-mode) ;Enabling CamelCase support for editing commands(like forward-word, backward-word, etc) in nREPL
 (add-hook 'cider-repl-mode-hook 'paredit-mode) ;Enable paredit in nRepl buffer
 (add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode) ; rainbow delimiters
