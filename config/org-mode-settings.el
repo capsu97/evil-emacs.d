dbf9a216ceca86986f6c8c27a74cbc48fd65b449
Okke Tijhuis <o.tijhuis@gmail.com>
2014-04-13 21:29:11 +0200

    update org-mode settings

--- config/org-mode-settings.el
+++ config/org-mode-settings.el
@@ -5,7 +5,27 @@
       '(("INPROGRESS" . (:foreground "blue" :weight bold)))) ; add inprogress keyword
 ;;(setq org-agenda-files (list "~/Dropbox/org/personal.org")) ; set default agenda file
 
+(setq org-log-done t) ; log completed items. adds timestamp and lets org-agenda mark it
+(setq org-todo-keywords
+      '((sequence "TODO" "INPROGRESS" "DONE")))
+(setq org-todo-keyword-faces
+      '(("INPROGRESS" . (:foreground "blue" :weight bold)))) ; add inprogress keyword
+;;(setq org-agenda-files (list "~/Dropbox/org/personal.org")) ; set default agenda file
+
 (setq org-confirm-babel-evaluate nil ; stop asking if you are sure you want to evaluate a src block
+      org-src-fontify-natively t ; inside src block use the colors like the major mode of the src type
+      org-src-tab-acts-natively t ; inside a src block let tab act like it was in major mode of the src type
+      org-use-speed-commands t ; enable single-letter commands at beginning of headline or code block, press ? at beginning of headline to see all the commands
+      )
+
+(require 'ob)
+
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (sh . t)))
+
+;; make it possible to embed clojure code in org documents with org-babel(setq org-confirm-babel-evaluate nil ; stop asking if you are sure you want to evaluate a src block
       org-src-fontify-natively t ; inside src block use the colors like the major mode of the src type
       org-src-tab-acts-natively t ; inside a src block let tab act like it was in major mode of the src type
       org-use-speed-commands t ; enable single-letter commands at beginning of headline or code block, press ? at beginning of headline to see all the commands
