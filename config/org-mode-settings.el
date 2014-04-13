6ffde0bdd4df143c9fdc193194693dcb33885e8a
Okke Tijhuis <o.tijhuis@gmail.com>
2014-04-13 21:27:56 +0200

    update org-mode settings

--- config/org-mode-settings.el
+++ config/org-mode-settings.el
@@ -5,13 +5,20 @@
       '(("INPROGRESS" . (:foreground "blue" :weight bold)))) ; add inprogress keyword
 ;;(setq org-agenda-files (list "~/Dropbox/org/personal.org")) ; set default agenda file
 
-;; make it possible to embed clojure code in org documents with org-babel
+(setq org-confirm-babel-evaluate nil ; stop asking if you are sure you want to evaluate a src block
+      org-src-fontify-natively t ; inside src block use the colors like the major mode of the src type
+      org-src-tab-acts-natively t ; inside a src block let tab act like it was in major mode of the src type
+      org-use-speed-commands t ; enable single-letter commands at beginning of headline or code block, press ? at beginning of headline to see all the commands
+      )
+
 (require 'ob)
 
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((sh . t)))
+ '((emacs-lisp . t)
+   (sh . t)))
 
+;; make it possible to embed clojure code in org documents with org-babel
 (add-to-list 'org-babel-tangle-lang-exts '("clojure" . "clj"))
 
 (defvar org-babel-default-header-args:clojure
@@ -20,8 +27,5 @@
 (defun org-babel-execute:clojure (body params)
   (lisp-eval-string body)
   "Done!")
-
-(setq org-src-fontify-natively t)
-(setq org-confirm-babel-evaluate nil)
 
 (provide 'org-mode-settings)
