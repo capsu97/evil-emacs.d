10e6534af990bbb1f7ba1a8544fc7b97196075db
Okke Tijhuis <okke.tijhuis@eno.nl>
2014-04-22 17:13:32 +0200

    update config

--- config/auto-complete-settings.el
+++ config/auto-complete-settings.el
@@ -11,17 +11,24 @@
 ;; Auto-complete
 (add-hook 'after-init-hook 'global-company-mode)
 
-;;(eval-after-load 'company '(add-to-list 'company-backends 'company-elisp))
-;;(eval-after-load 'company '(add-to-list 'company-backends 'company-capf))
+;; yasnippet backend shadows other completions, see https://github.com/company-mode/company-mode/blob/master/company-yasnippet.el for solutions
 ;;(eval-after-load 'company '(add-to-list 'company-backends 'company-yasnippet))
+
 ;;(eval-after-load 'company '(add-to-list 'company-backends 'company-files))
 (eval-after-load 'company '(add-to-list 'company-transformers 'company-sort-by-occurrence))
+
+;;(setq company-backends '(company-dabbrev (company-keywords company-dabbrev-code) company-files))
+;;(setq company-begin-commands '(self-insert-command org-self-insert-command c-electric-lt-gt c-electric-colon))
 
 (setq company-idle-delay 0.3)
 (setq company-tooltip-limit 20)
 (setq company-minimum-prefix-length 2)
 (setq company-echo-delay 0)
 (setq company-auto-complete nil)
+(setq company-selection-wrap-around t)
+(setq company-show-numbers nil)
+(setq company-dabbrev-other-buffers t)
+;;(setq company-auto-complete-chars nil)
 
 ;; org-mode completions
 (defun my-pcomplete-capf ()
